c5f90f68042b29d2e09102f1ec2307bbf3983c7d
坂口諒輔 <gucchi0403@gmail.com>
2017-08-04 18:00:47 +0900

    Practice05完了しました。

--- Practice/src/practice05/PTra05_04.java
+++ Practice/src/practice05/PTra05_04.java
@@ -21,18 +21,41 @@ public class PTra05_04 {
 		 * ●0～6が入力されている場合は、各曜日を出力した後プログラムを終了します
 		 *
 		 */
+		int i=0;
 
+		while(i==0) {
 		// ①
-		System.out.println("0～6の整数を入力してください");
+			System.out.println("0～6の整数を入力してください");
 
-		String line = scanner.nextLine();
+			String line = scanner.nextLine();
 
-		switch(line) {
-			case "0":
-				System.out.println("日曜");
-				break;
-			case "1":
-				System.out.println("月曜");
+			switch(line) {
+				case "0":
+					System.out.println("日曜");
+					break;
+				case "1":
+					System.out.println("月曜");
+					break;
+				case "2":
+					System.out.println("火曜");
+					break;
+				case "3":
+					System.out.println("水曜");
+					break;
+				case "4":
+					System.out.println("木曜");
+					break;
+				case "5":
+					System.out.println("金曜");
+					break;
+				case "6":
+					System.out.println("土曜");
+					break;
+				default:
+					i--;
+					break;			
+			}
+			i++;
 		}
 	}
 }

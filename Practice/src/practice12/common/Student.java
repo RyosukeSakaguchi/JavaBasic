9ce974574b50b0d2d8683e086aca9a6914343a36
坂口諒輔 <gucchi0403@gmail.com>
2017-08-07 18:34:49 +0900

    Practice12が完了しました。

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -29,4 +29,34 @@ public class Student {
 
 	/** パスワード */
 	public String password;
+	
+	public int getId() {
+        return this.studentId;
+    }
+	public String getStName() {
+        return this.studentName;
+    }
+	public String getCoName() {
+        return this.companyName;
+    }
+	public String getClName() {
+        return this.className;
+    }
+	public String getMail() {
+        return this.mail;
+    }
+	public String getPass() {
+        return this.password;
+    }
+	
+	public void setName(int studentId, String studentName, String companyName, String className, String mail, String password) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.companyName = companyName;
+        this.className = className;
+        this.mail = mail;
+        this.password = password;
+    }
+
+
 }
